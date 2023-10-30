318a54b4f135a1f347716cc5cea28d46f1d41ad4
sultanr31 <83525246+MSultan-2057051019@users.noreply.github.com>
2023-10-30 09:38:57 +0700

    Upload

---
+++ index.php
@@ -0,0 +1,355 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="mystyle.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+ 
+        <div class="container">
+            <!-- <a class="navbar-brand" href="#">Dinas Perpus takaan dan Kearsipan</a> -->
+
+        
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav mx-auto">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link active" href="#">Profile</a>
+                <a class="nav-item nav-link active" href="#">Perpustakaan</a>
+                <a class="nav-item nav-link active" href="#">Berita</a>
+                <a class="nav-item nav-link active" href="#">Galeri</a>
+                </div>
+            </div>
+      </div>
+    </nav>
+
+    <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+        <img class="logo" src="assets/Logo.png" width="120" alt="Dinas Perpustakaan dan Kearsipan">
+            <h1 class="display-4">Dinas Perpustakaan dan Kearsipan</h1>
+            <p class="caption">Cari Berkas yang ingin anda temukan, hanya dengan satu kali klik, dimanapun dan kapanpun!</p>
+            <a href="" class="button-30" role="button">Search&nbsp;<i>&#128269;</i></a>
+        </div>
+    </div>
+
+    <!-- Panel -->
+    <!-- <div class="row justify-content-center">
+        <div class="col-9 info-panel">
+          <div class="row justify-content-center">
+            <div class="col-lg-4">
+              <div class="row justify-content-center">
+                <div class="col-4 d-flex justify-content-center align-self-center">
+                  <img src="" alt="">
+                </div>
+                <div class="col-6 panel-text">
+                  <h4>24 Hours</h4>
+                  <p>Lorem ipsum dolor sit amet sin.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="row justify-content-center">
+                <div class="col-4 d-flex justify-content-center align-self-center">
+                  <img src="" alt="">
+                </div>
+                <div class="col-6 panel-text">
+                  <h4>High-Res</h4>
+                  <p>Lorem ipsum dolor sit amet sin.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="row justify-content-center">
+                <div class="col-4 d-flex justify-content-center align-self-center">
+                  <img src="" alt="">
+                </div>
+                <div class="col-6 panel-text">
+                  <h4>Security</h4>
+                  <p>Lorem ipsum dolor sit amet sin.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> -->
+
+      <!-- Akhir Test -->
+
+    <!-- Cards -->
+
+
+    <h1 class="text-center card-display">Berita Terbaru</h1>
+    <div class="container cards">
+      <div class="card-columns">
+        <div class="card custom-card" style="border: none;">
+          <a href="#">
+          <img class="card-img-top" src="https://images.unsplash.com/photo-1535025639604-9a804c092faa?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6cb0ceb620f241feb2f859e273634393&auto=format&fit=crop&w=500&q=80" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Lorem ipsum dolor sit amet.</h5>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium ad alias, aliquid amet aspernatur atque culpa cum debitis dicta doloremque, dolorum ea eos et excepturi explicabo facilis harum illo impedit incidunt laborum laudantium...
+            </p>
+            <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>&nbsp;1000&nbsp;&nbsp;&nbsp;<i class="fas fa-calendar-alt"></i>&nbsp;Jan 20, 2018</small></p>
+        </div>
+          </a>
+        </div>
+        <div class="card custom-card" style="border: none;">
+          <a href="#">
+          <img class="card-img-top" src="https://images.unsplash.com/photo-1472076638602-b1f8b1ac0b4a?ixlib=rb-0.3.5&s=63c9de7246b535be56c8eaff9b87dd89&auto=format&fit=crop&w=500&q=80" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, doloremque!</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio iusto maxime nemo omnis praesentium similique.</p>
+            <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>&nbsp;1000&nbsp;&nbsp;&nbsp;<i class="fas fa-calendar-alt"></i>&nbsp;Jan 20, 2018</small></p>
+          </div>
+          </a>
+        </div>
+        <div class="card custom-card" style="border: none;">
+          <a href="#">
+          <img class="card-img-top" src="https://images.unsplash.com/photo-1535086181678-5a5c4d23aa7d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=34c86263bec2c8f74ceb74e9f4c5a5fc&auto=format&fit=crop&w=500&q=80" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur.</h5>
+            <p class="card-text">Amet commodi deleniti enim laboriosam odio placeat praesentium quis ratione rerum suscipit.</p>
+            <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>&nbsp;1000&nbsp;&nbsp;&nbsp;<i class="fas fa-calendar-alt"></i>&nbsp;Jan 20, 2018</small></p>
+          </div>
+          </a>
+        </div>
+        <div class="card custom-card" style="border: none;">
+          <a href="#">
+          <img class="card-img-top" src="https://images.unsplash.com/photo-1535074153497-b08c5aa9c236?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d2aaf944a59f16fe1fe72f5057b3a7dd&auto=format&fit=crop&w=500&q=80" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur.</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>&nbsp;1000&nbsp;&nbsp;&nbsp;<i class="fas fa-calendar-alt"></i>&nbsp;Jan 20, 2018</small></p>
+          </div>
+          </a>
+        </div>
+        <div class="card custom-card" style="border: none;">
+          <a href="#">
+          <img class="card-img-top" src="https://images.unsplash.com/photo-1535124406821-d2848dfbb25c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=98c434d75b44c9c23fc9df2a9a77d59f&auto=format&fit=crop&w=500&q=80" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Lorem ipsum dolor sit amet.</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, consequatur culpa cumque deserunt dignissimos error explicabo fugiat harum ipsam magni minus mollitia nemo perferendis qui repellendus rerum saepe vel voluptate voluptatem voluptatum!
+              Aperiam, labore, molestiae!..</p>
+            <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>&nbsp;1000&nbsp;&nbsp;&nbsp;<i class="fas fa-calendar-alt"></i>&nbsp;Jan 20, 2018</small></p>
+          </div>
+          </a>
+        </div>
+        <div class="card custom-card" style="border: none;">
+          <a href="#">
+          <img class="card-img-top" src="https://images.unsplash.com/photo-1508015926936-4eddcc6d4866?ixlib=rb-0.3.5&s=10b3a8717ab609be8d7786cab50c4e0b&auto=format&fit=crop&w=500&q=80" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque commodi debitis eaque explicabo fuga maiores necessitatibus, neque omnis optio vel!</p>
+            <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>&nbsp;1000&nbsp;&nbsp;&nbsp;<i class="fas fa-calendar-alt"></i>&nbsp;Jan 20, 2018</small></p>
+          </div>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="assets/car1.jpg" style="background: #C4C4C4;" alt="First slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Slide 1</h5>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="assets/car2.jpg" style="background: #C4C4C4;" alt="Second slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Slide 2</h5>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="assets/car1.jpg" style="background: #C4C4C4;" alt="Third slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Slide 3</h5>
+          <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit impedit reiciendis quia laborum, rerum vitae corrupti magnam dolor doloremque sunt quisquam iusto quaerat ad et quo nobis, nesciunt eos ratione!</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" ariahidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  <div class="row workingspace">
+    <div class="col-lg-6">
+      <img src="assets/car3.jpg" alt="workingspace" class="img-fluid">
+    </div>
+    <div class="col-lg-4">
+      <h4 class="card-display">Tentang Kami</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nesciunt ipsa atque qui molestias fuga voluptate vel eligendi repellat nam porro perferendis excepturi harum dolores adipisci inventore, ab, tenetur delectus.</p>
+    </div>
+  </div>
+
+
+    <!-- <div class="row">
+      <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src=""
+            class="w-100"
+          />
+          <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal1">
+            <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 mb-4 mb-lg-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src=""
+            class="w-100"
+          />
+          <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal2">
+            <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-4 mb-4 mb-lg-0">
+        <div
+          class="bg-image hover-overlay ripple shadow-1-strong rounded"
+          data-ripple-color="light"
+        >
+          <img
+            src=""
+            class="w-100"
+          />
+          <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal3">
+            <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
+          </a>
+        </div>
+      </div>
+    </div> -->
+
+
+  <footer class="text-center text-lg-start text-dark stripe-3">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row my-4">
+        <!--Grid column-->
+        <div class="col-lg-5 col-md-6 mb-4 mb-md-0">
+          <img src="assets/logo.png" height="120" alt="" loading="lazy" />
+          <p class="text-center text-logo mt-3">Dinas Perpustakaan dan Kearsipan <br>Lampung Selatan</p>
+          <ul class="list-unstyled d-flex flex-row justify-content-center">
+            <li>
+              <a class="text-dark px-2" href="#!">
+                <i class="fab fa-facebook-square"></i>
+              </a>
+            </li>
+            <li>
+              <a class="text-dark px-2" href="#!">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a class="text-dark px-2" href="#!">
+                <i class="fab fa-youtube"></i>
+              </a>
+            </li>
+          </ul>
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-left text-title mb-4" style="width: 28%;">Lainnya</h5>
+
+          <ul class="list-unstyled text-left">
+            <li class="mb-2">
+              <a href="#!" class="text-dark">General information</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-dark">About the shelter</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-dark">Statistic data</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-dark">Job</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-dark">Tenders</a>
+            </li>
+            <li class="mb-2">
+              <a href="#!" class="text-dark">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-left text-title mb-4">Kontak kami</h5>
+
+          <ul class="list-unstyled text-left"> 
+            <li>
+              <p><i class="fas fa-map-marker-alt pe-2"></i>&nbsp;Warsaw, 57 Street, Poland</p>
+            </li>
+            <li>
+              <p><i class="fas fa-phone pe-2"></i>&nbsp;+ 01 234 567 89</p>
+            </li>
+            <li>
+              <p><i class="fas fa-envelope pe-2 mb-0"></i>&nbsp;contact@example.com</p>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center text-white p-3" style="background-color: black">
+      © 2020 All Rights Reserved.
+    </div>
+    <!-- Copyright -->
+  </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
